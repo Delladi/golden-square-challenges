5db99456d04c0d32f46e32034a31da088a02d671
Della Izadi <delaram.ae@gmail.com>
2023-05-15 15:04:46 +0100

    Passed the second test true statement

--- lib/grammar_design.rb
+++ lib/grammar_design.rb
@@ -1,4 +1,5 @@
 def grammar_design(text)
 
     fail "Error: This is an empty text!" if text.empty?
+    return true
 end
--- spec/grammar_design_spec.rb
+++ spec/grammar_design_spec.rb
@@ -6,4 +6,10 @@ RSpec.describe "program that checks grammar design" do
         expect{ grammar_design("") }.to raise_error "Error: This is an empty text!"
         end
     end
+
+    context "string starting with capital letter and full stop" do
+        it "returns true" do 
+        expect(grammar_design("Hello world.")).to eq true
+        end
+    end
 end
