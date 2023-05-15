7b8dca85ed13f26febd5a0e44878b731f8043696
Della Izadi <delaram.ae@gmail.com>
2023-05-15 16:38:19 +0100

    Added a method for correct sentense with an exclamation mark at the end

--- lib/grammar_design.rb
+++ lib/grammar_design.rb
@@ -2,8 +2,8 @@ def grammar_design(text)
 
     fail "Error: This is an empty text!" if text.empty?
     first_letter_is_uppercase = text[0] == text[0].upcase
-    last_character_is_fullstop = text[-1] == "."
-    if first_letter_is_uppercase && last_character_is_fullstop 
+    last_character_is_valid = [".", "!"].include? text[-1]
+    if first_letter_is_uppercase && last_character_is_valid
         return true
     else
         return false
