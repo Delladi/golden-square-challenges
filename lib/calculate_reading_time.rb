d90c14b8bc511bf07381c409d0cb7bb2b67a59a2
Della Izadi <delaram.ae@gmail.com>
2023-05-15 13:57:30 +0100

    added method to take text with 200 words

--- lib/calculate_reading_time.rb
+++ lib/calculate_reading_time.rb
@@ -2,7 +2,7 @@ def calculate_reading_time(text)
     words = text.split(" ")
     if words.length == 0 
         return 0
-    elsif words.length < 200 
+    elsif words.length <= 200 
         return 1
     end
 end
