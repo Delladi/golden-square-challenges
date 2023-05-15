621dd8e6d6311bddb33b284f95163b70e8e411b1
Della Izadi <delaram.ae@gmail.com>
2023-05-15 15:41:11 +0100

    Added a method last_character_is_fullstop

--- lib/grammar_design.rb
+++ lib/grammar_design.rb
@@ -1,10 +1,11 @@
 def grammar_design(text)
 
     fail "Error: This is an empty text!" if text.empty?
-    first_letter_is_capital = text[0] != text[0].upcase
-    if first_letter_is_capital    
-        return false
-    else
+    first_letter_is_uppercase = text[0] == text[0].upcase
+    last_character_is_fullstop = text[-1] == "."
+    if first_letter_is_uppercase && last_character_is_fullstop 
         return true
+    else
+        return false
     end
 end
