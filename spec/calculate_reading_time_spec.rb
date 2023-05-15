bc09e898b984f90ec6050743ec62b2b5168c8333
Della Izadi <delaram.ae@gmail.com>
2023-05-15 13:36:26 +0100

    Adding if statement and passed the second test

--- spec/calculate_reading_time_spec.rb
+++ spec/calculate_reading_time_spec.rb
@@ -7,4 +7,11 @@ RSpec.describe "it calculates the reading time method" do
             expect(result).to eq 0
         end 
     end 
+
+        context "given a text for one word" do
+            it "returns one" do
+                result = calculate_reading_time("one")
+                expect(result).to eq 1
+        end 
+    end
 end
