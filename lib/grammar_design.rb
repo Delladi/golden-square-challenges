f42840daedecb578287674c5ffba32b87018ae2e
Della Izadi <delaram.ae@gmail.com>
2023-05-15 15:23:44 +0100

    created a method if the first letter is capital

--- lib/grammar_design.rb
+++ lib/grammar_design.rb
@@ -1,5 +1,10 @@
 def grammar_design(text)
 
     fail "Error: This is an empty text!" if text.empty?
-    return true
+    first_letter_is_capital = text[0] != text[0].upcase
+    if first_letter_is_capital    
+        return false
+    else
+        return true
+    end
 end
