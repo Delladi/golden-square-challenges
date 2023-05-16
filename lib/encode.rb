3a9174bc193c1b95bd8cade73450836da181d740
Della Izadi <delaram.ae@gmail.com>
2023-05-16 14:28:02 +0100

    encode debugging passed

---
+++ lib/encode.rb
@@ -0,0 +1,25 @@
+def encode(plaintext, key)
+    cipher = key.chars.uniq + (('a'..'z').to_a - key.chars)
+    #extra dot between a and z
+    ciphertext_chars = plaintext.chars.map do |char|
+      (65 + cipher.find_index(char)).chr
+    end
+    return ciphertext_chars.join
+  end
+  
+  def decode(ciphertext, key)
+    cipher = key.chars.uniq + (('a'..'z').to_a - key.chars)
+    plaintext_chars = ciphertext.chars.map do |char|
+      cipher[char.ord - 65]
+      # this array should be the other way around. so instead of [65 - char.ord] it should be 
+    end
+    return plaintext_chars.join
+  end
+  
+  # Intended output:
+  #
+  # > encode("theswiftfoxjumpedoverthelazydog", "secretkey")
+  # => "EMBAXNKEKSYOVQTBJSWBDEMBPHZGJSL"
+  #
+  # > decode("EMBAXNKEKSYOVQTBJSWBDEMBPHZGJSL", "secretkey")
+  # => "theswiftfoxjumpedoverthelazydog"
