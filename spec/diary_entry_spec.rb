83aa4fc376b2b0d3690dc9dd9fc743f462db408d
Della Izadi <delaram.ae@gmail.com>
2023-05-17 14:03:24 +0100

    Added the second passing test

--- spec/diary_entry_spec.rb
+++ spec/diary_entry_spec.rb
@@ -2,7 +2,8 @@ require 'diary_entry'
 
 RSpec.describe DiaryEntry do
     it "constructs" do
-        diary_entry = DiaryEntry.new("my_title", "my_content")
+        diary_entry = DiaryEntry.new("my_title", "my_contents")
         expect(diary_entry.title).to eq "my_title"
+        expect(diary_entry.contents).to eq "my_contents"
     end
 end
