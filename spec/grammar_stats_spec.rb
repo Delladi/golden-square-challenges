d31fd77a3e7334c2f796d12d2adb6eeb7fd0807e
Della Izadi <delaram.ae@gmail.com>
2023-05-18 14:13:44 +0100

    Added a test for checking an empty string and raising an error

--- spec/grammar_stats_spec.rb
+++ spec/grammar_stats_spec.rb
@@ -9,7 +9,7 @@ RSpec.describe GrammarStats do
 
         it "raises error if the string is empty" do
             stats = GrammarStats.new 
-            expect{stats.check("")}.to_raise error "Error! This string is empty."
+            expect{ stats.check("")}.to raise_error "Error! This string is empty."
         end
     end
 end
