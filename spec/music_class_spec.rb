eb1e4d73e1ee3a0a738bf2d07fa493d16a0ccb9e
Della Izadi <delaram.ae@gmail.com>
2023-05-18 21:55:14 +0100

    Added a test to add one track when given a track

--- spec/music_class_spec.rb
+++ spec/music_class_spec.rb
@@ -8,5 +8,13 @@ RSpec.describe Music do
       expect(music_tracks.list).to eq []
       end
     end
+
+    context "given one track list" do
+        it "returns the music track" do
+        music_tracks = Music.new
+        music_tracks.add("Don't stop me now")
+        expect(music_tracks.list).to eq ["Don't stop me now"]
+        end
+      end
   end 
 end
