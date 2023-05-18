ee1fbc30a4d88beb399f08428a81c26c15c41f6f
Della Izadi <delaram.ae@gmail.com>
2023-05-18 14:46:35 +0100

    Added passing tests for checking percentage

--- spec/grammar_stats_spec.rb
+++ spec/grammar_stats_spec.rb
@@ -2,16 +2,30 @@ require 'grammar_stats'
 
 RSpec.describe GrammarStats do
     describe "#check" do
+         it "raises error if the string is empty" do
+             stats = GrammarStats.new 
+               expect{ stats.check("") }.to raise_error "Error! This string is empty."
+         end
+
+
         it "returns true once it checks the letters" do
             stats = GrammarStats.new 
             expect(stats.check("Hello world.")).to eq true
         end
+    end
 
-        it "raises error if the string is empty" do
+    describe "#percentage_good" do
+        it "Returns as an integer the percentage of texts checked so far that passed" do
             stats = GrammarStats.new 
-            expect{ stats.check("")}.to raise_error "Error! This string is empty."
+            stats.check("Hello world.")
+            stats.check("Hello world!")
+            stats.check("Hello world?")
+            stats.check("Hello world")
+            stats.check("hello world")
+            expect(stats.percentage_good).to eq 60
         end
     end
+
 end
 
 
