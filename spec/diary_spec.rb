0d8796d59d1083ec3379b8b79ba7a43d87bed7f3
Della Izadi <delaram.ae@gmail.com>
2023-05-19 18:08:11 +0100

    Updated

--- spec/diary_spec.rb
+++ spec/diary_spec.rb
@@ -4,6 +4,7 @@ RSpec.describe Diary do
     context "initially" do
         it "has an empty list of entries" do
             diary = Diary.new
+            diary_entry1 = DiaryEntry.new("my title", "my contents")
             expect(diary.all).to eq []
         end
     end
